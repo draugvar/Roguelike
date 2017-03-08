74fc63dec054d9f938e7d1e29644a745f2a8d5fc
draugvar <draugvar@gmail.com>
2017-03-08 11:36:05 +0100

    adding UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
-
-using System.Collections.Generic;       //Allows us to use Lists. 
+using System.Collections.Generic;		//Allows us to use Lists. 
+using UnityEngine.UI;					//Allows us to use UI.
 
 public class GameManager : MonoBehaviour
 {
-	public float levelStartDelay = 2f;                      //Time to wait before starting level, in seconds.
-	public float turnDelay = 0.1f;                          //Delay between each Player turn.
-	public int playerFoodPoints = 100;                      //Starting value for Player food points.
-	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
-	[HideInInspector] public bool playersTurn = true;       //Boolean to check if it's players turn, hidden in inspector but public.
+	public float levelStartDelay = 2f;						//Time to wait before starting level, in seconds.
+	public float turnDelay = 0.1f;							//Delay between each Player turn.
+	public int playerFoodPoints = 100;						//Starting value for Player food points.
+	public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
+	[HideInInspector] public bool playersTurn = true;		//Boolean to check if it's players turn, hidden in inspector but public.
 
 
-	private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
-	private int level = 1;                                  //Current level number, expressed in game as "Day 1".
-	private List<Enemy> enemies;                          //List of all Enemy units, used to issue them move commands.
-	private bool enemiesMoving;                             //Boolean to check if enemies are moving.
+	private Text levelText;									//Text to display current level number.
+	private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
+	private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
+	private int level = 1;									//Current level number, expressed in game as "Day 1".
+	private List<Enemy> enemies;							//List of all Enemy units, used to issue them move commands.
+	private bool enemiesMoving;								//Boolean to check if enemies are moving.
+	private bool doingSetup = true;							//Boolean to check if we're setting up board, prevent Player from moving during setup.
 
 
 
@@ -33,7 +37,7 @@ public class GameManager : MonoBehaviour
 		else if (instance != this)
 
 			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-			Destroy(gameObject);    
+			Destroy(gameObject);	
 
 		//Sets this to not be destroyed when reloading scene
 		DontDestroyOnLoad(gameObject);
@@ -48,18 +52,43 @@ public class GameManager : MonoBehaviour
 		InitGame();
 	}
 
-	//This is called each time a scene is loaded.
-	void OnLevelWasLoaded(int index)
+	//this is called only once, and the paramter tell it to be called only after the scene was loaded
+	//(otherwise, our Scene Load callback would be called the very first load, and we don't want that)
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+	static public void CallbackInitialization()
 	{
-		//Add one to our level number.
-		level++;
-		//Call InitGame to initialize our level.
-		InitGame();
+		//register the callback to be called everytime the scene is loaded
+		SceneManager.sceneLoaded += OnSceneLoaded;
 	}
+
+	//This is called each time a scene is loaded.
+	static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+	{
+		instance.level++;
+		instance.InitGame();
+	}
+
 
 	//Initializes the game for each level.
 	void InitGame()
 	{
+		//While doingSetup is true the player can't move, prevent player from moving while title card is up.
+		doingSetup = true;
+
+		//Get a reference to our image LevelImage by finding it by name.
+		levelImage = GameObject.Find("LevelImage");
+
+		//Get a reference to our text LevelText's text component by finding it by name and calling GetComponent.
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+
+		//Set the text of levelText to the string "Day" and append the current level number.
+		levelText.text = "Day " + level;
+
+		//Set levelImage to active blocking player's view of the game board during setup.
+		levelImage.SetActive(true);
+
+		//Call the HideLevelImage function with a delay in seconds of levelStartDelay.
+		Invoke("HideLevelImage", levelStartDelay);
 
 		//Clear any Enemy objects in our List to prepare for next level.
 		enemies.Clear();
@@ -70,11 +99,21 @@ public class GameManager : MonoBehaviour
 	}
 
 
+	//Hides black image used between levels
+	void HideLevelImage()
+	{
+		//Disable the levelImage gameObject.
+		levelImage.SetActive(false);
+
+		//Set doingSetup to false allowing player to move again.
+		doingSetup = false;
+	}
+
 	//Update is called every frame.
 	void Update()
 	{
 		//Check that playersTurn or enemiesMoving or doingSetup are not currently true.
-		if(playersTurn || enemiesMoving)
+		if(playersTurn || enemiesMoving || doingSetup)
 
 			//If any of these are true, return and do not start MoveEnemies.
 			return;
@@ -94,9 +133,11 @@ public class GameManager : MonoBehaviour
 	//GameOver is called when the player reaches 0 food points
 	public void GameOver()
 	{
+		//Set levelText to display number of levels passed and game over message
+		levelText.text = "After " + level + " days, you starved.";
 
 		//Enable black background image gameObject.
-		//levelImage.SetActive(true);
+		levelImage.SetActive(true);
 
 		//Disable this GameManager.
 		enabled = false;
